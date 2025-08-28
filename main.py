485cf57f459c8d180ce25f2d7b6f4fb539b8821f
donald kiarie <donaldkiarie768@gmail.com>
2025-08-28 13:04:55 +0300

    add file

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 try:
     from lib.cli import cli
-    print("🎵 Music App CLI Starting... 🎵")
+    print(" Music App CLI Starting... ")
     if __name__ == '__main__':
         cli()
 except ImportError as e:
