4d5c88ee0c771dbcabbb9727922e4e3f2df2c380
donald kiarie <donaldkiarie768@gmail.com>
2025-08-28 16:04:12 +0300

    add file

--- main.py
+++ main.py
@@ -2,14 +2,15 @@
 import sys
 import os
 
-
 sys.path.insert(0, os.path.dirname(__file__))
 
 try:
     from lib.cli import cli
-    print(" Music App CLI Starting... ")
+    print("🎵 Music App CLI Starting... ")
+    
     if __name__ == '__main__':
         cli()
+        
 except ImportError as e:
     print(f"Import Error: {e}")
     print("Make sure you're running from the project root directory")
